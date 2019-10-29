f13fcbc7248da544a74519c831b26ce5c13dbaa1
Fatih Bozik <bozikfatih@gmail.com>
2019-10-29 20:55:42 +0300

    Add Bundle properties file

---
+++ src/main/java/io/github/fatihbozik/licencegenerator/LicenceGeneratorBundle.java
@@ -0,0 +1,23 @@
+package io.github.fatihbozik.licencegenerator;
+
+import com.intellij.CommonBundle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+/**
+ * {@link ResourceBundle}/localization utils for the plugin.
+ *
+ * @author Fatih Bozik <bozikfatih@gmail.com>
+ * @since 0.1
+ */
+public class LicenceGeneratorBundle {
+    private static final String BUNDLE_NAME = "messages.LicenceGeneratorBundle";
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    @NotNull
+    public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key, Object... params) {
+        return CommonBundle.message(BUNDLE, key, params);
+    }
+}
