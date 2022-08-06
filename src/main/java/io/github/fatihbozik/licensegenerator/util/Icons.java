42a0496309316844859db31edc151dd5ee97ed4f
Fatih Bozik <bozikfatih@gmail.com>
2022-08-06 16:20:40 +0300

    Make compatible with Idea 2022.1.1 #15

--- src/main/java/io/github/fatihbozik/licensegenerator/util/Icons.java
+++ src/main/java/io/github/fatihbozik/licensegenerator/util/Icons.java
@@ -2,7 +2,7 @@ package io.github.fatihbozik.licensegenerator.util;
 
 import com.intellij.openapi.util.IconLoader;
 
-import javax.swing.Icon;
+import javax.swing.*;
 
 /**
  * {@link Icons} class that holds icon resources.
@@ -11,7 +11,7 @@ import javax.swing.Icon;
  */
 public final class Icons {
     /** General ignore icon. */
-    public static final Icon LICENSE = IconLoader.getIcon("/icons/license.png");
+    public static final Icon LICENSE = IconLoader.getIcon("/icons/license.png", Icons.class);
 
     /** Private constructor to prevent creating {@link Icons} instance. */
     private Icons() {
